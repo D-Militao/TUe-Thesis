352070d50f96e8d4cf11605127b2b2b9febcd05c
d-militao <daniel.militao95@gmail.com>
2021-10-18 23:15:51 +0200

    Aesthetic changes

--- tests/test_all_distance_sketch.py
+++ tests/test_all_distance_sketch.py
@@ -8,6 +8,7 @@ from .context import snap_util, load_data
 from .context import GraphSketch
 from .test_util import elapsed_time, elapsed_time_str
 
+
 def test_time_all_distance_sketch_node_ids(network, node_ids, k):
     # Create the sketch for the given k
     start_sketch = time()
@@ -16,7 +17,7 @@ def test_time_all_distance_sketch_node_ids(network, node_ids, k):
     sketch_construction_time = elapsed_time(start_sketch)
 
     # Perform estimates on the sketch
-    estimates = [] # snap.TFltV()
+    estimates = []  # snap.TFltV()
     start_est = time()
     for node_id in node_ids:
         estimates.append(graph_sketch.cardinality_estimation_node_id(node_id))
@@ -35,7 +36,7 @@ def test_memory_all_distance_sketch_node_ids(network, node_ids, k):
     sketch_size = after_sketch-before_sketch
 
     # Perform estimates on the sketch
-    estimates = [] # snap.TFltV()
+    estimates = []  # snap.TFltV()
     before_est, _ = tracemalloc.get_traced_memory()
     tracemalloc.reset_peak()
     for node_id in node_ids:
@@ -44,4 +45,3 @@ def test_memory_all_distance_sketch_node_ids(network, node_ids, k):
     total_estimation_size = after_est-before_est
 
     return sketch_size, sketch_peak, total_estimation_size, est_peak, estimates
-    
