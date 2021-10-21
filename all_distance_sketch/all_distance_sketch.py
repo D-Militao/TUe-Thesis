87b8c808b84dd175876ebdc5841f1f05c671e695
d-militao <daniel.militao95@gmail.com>
2021-10-21 16:59:30 +0200

    Changed data structure; added save and load methods.

--- all_distance_sketch/all_distance_sketch.py
+++ all_distance_sketch/all_distance_sketch.py
@@ -2,6 +2,8 @@ import random
 import math
 from pprint import pprint
 from dataclasses import dataclass
+import pickle
+import json
 
 import snap
 
@@ -17,34 +19,32 @@ class GraphSketch:
             self.tranposed_graph = snap_util.transpose_graph(graph)
         self.k = k
         self.node_ids = snap.TIntV()
-        self.rankings = {}
-        self.node_sketches = {}
-        self.neighborhoods = {}
+        self.rankings = snap.TIntFltH()
+        self.node_sketches = snap.TIntIntPrVH()
 
+        random.seed(42)
         for NI in graph.Nodes():
             node_id = NI.GetId()
             self.node_ids.append(node_id)
             self.node_sketches[node_id] = snap.TIntPrV()
-
-        random.seed(42)
-        for node_id in self.node_ids:
+            
             rank = random.uniform(0, 1)
-            self.rankings[node_id] = rank
+            self.rankings[node_id] = rank            
 
     def calculate_graph_sketch(self):
         for rankee_node_id, rankee_rank in sorted(self.rankings.items(), key=lambda item: item[1]):
+            print(rankee_rank)
             queue = snap.TIntV()
             queue.append(rankee_node_id)
-            distances = {}
+            distances = snap.TIntH()
             distances[rankee_node_id] = 0
-            colors = {}
-            colors[rankee_node_id] = 1
-            parents = {}
-            parents[rankee_node_id] = None
+            # visited_nodes = set()
+            # visited_nodes.add(rankee_node_id)
+            visited_nodes = snap.TIntSet()
+            visited_nodes.AddKey(rankee_node_id)
 
             while not queue.Empty():
-                pop_index = queue.Len() - 1
-                parent_node_id = queue.pop(pop_index)
+                parent_node_id = queue.pop(0)
                 parent_distance = distances[parent_node_id]
 
                 # Do the insertion checks
@@ -77,15 +77,10 @@ class GraphSketch:
                     NI = self.tranposed_graph.GetNI(parent_node_id)
                     out_node_ids = NI.GetOutEdges()
                     for out_node_id in out_node_ids:
-                        if out_node_id not in colors:
-                            colors[out_node_id] = 0
+                        if out_node_id not in visited_nodes:
+                            visited_nodes.AddKey(out_node_id)
                             distances[out_node_id] = parent_distance + 1
-                            parents[out_node_id] = parent_node_id
                             queue.append(out_node_id)
-                
-                # whether we inserted and continued the search or we didn't and 
-                # pruned, we color the node
-                colors[parent_node_id] = 1
 
     def cardinality_estimation_node_id(self, node_id, dist=math.inf):
         sketch = self.node_sketches[node_id]
@@ -105,6 +100,23 @@ class GraphSketch:
             size_estimate = neighborhood_size
 
         return size_estimate
+
+    def save(self, filename):
+        sketch_bin = filename+f'_k={self.k}_sketch.bin'
+        ranking_bin = filename+f'_k={self.k}_ranking.bin'
+        
+        self.node_sketches.Save(snap.TFOut(sketch_bin))
+        self.rankings.Save(snap.TFOut(ranking_bin))
+    
+    def load(self, filename):
+        try:
+            sketch_bin = filename+f'_k={self.k}_sketch.bin'
+            ranking_bin = filename+f'_k={self.k}_ranking.bin'
+            
+            self.node_sketches.Load(snap.TFIn(sketch_bin))
+            self.rankings.Load(snap.TFIn(ranking_bin))
+        except RuntimeError as err:
+            print(err)
 
     # ! ------------------------------------------------------------- ! #
     # ! Functions below are no longer in use and will be deleted soon ! #
@@ -140,33 +152,3 @@ class GraphSketch:
                 if insert:
                     pair = snap.TIntPr(dijkstra_dist, rankee_node_id)
                     self.node_sketches[dijkstra_node_id].AddSorted(pair, False)
-
-    def calculate_neighborhoods(self):
-        for node_id, sketch in self.node_sketches.items():
-            neighborhood = {}
-            for pair in sketch:
-                pair_dist = pair.GetVal1()
-                pair_node_id = pair.GetVal2()
-                pair_ranking = self.rankings[pair_node_id]
-                neighborhood.setdefault(pair_dist, snap.TFltV()).AddSorted(
-                    pair_ranking, True)
-            self.neighborhoods[node_id] = neighborhood
-
-    def cardinality_estimation_neighborhood(self, node_id, query_dist=math.inf):
-        neighborhood = self.neighborhoods[node_id]
-        all_neighbors = snap.TFltV()
-        counter = 0
-        for dist, neighbors in neighborhood.items():
-            if dist > query_dist:
-                break
-            counter += neighbors.Len()
-            all_neighbors.AddVMerged(neighbors)
-
-        neighborhood_size = all_neighbors.Len()
-        if neighborhood_size >= self.k:
-            tau = all_neighbors[self.k - 1]
-            size_estimate = (self.k - 1) / tau
-        else:
-            size_estimate = neighborhood_size
-
-        return size_estimate
